305ae51889b77245662a2604f2e39e187e0bdafc
Riichan04 <22130274@st.hcmuaf.edu.vn>
2023-11-30 21:07:01 +0700

    Task 1 + 2 but had written in OOP :D

--- BNode.java
+++ BNode.java
@@ -18,7 +18,7 @@ public class BNode<E extends Comparable<E>> {
         this.left = null;
         this.right = null;
     }
-    
+
     public E getData() {
         return data;
     }
@@ -43,19 +43,107 @@ public class BNode<E extends Comparable<E>> {
         this.right = right;
     }
 
+    //for add method
+    public void add(BNode<E> root, E e) {
+        if (root.getData().compareTo(e) > 0) {
+            if (root.getLeft() == null) root.setLeft(new BNode<>(e));
+            else add(root.getLeft(), e);
+        } else {
+            if (root.getRight() == null) root.setRight(new BNode<>(e));
+            else add(root.getRight(), e);
+        }
+    }
 
+    public BNode<E> search(BNode<E> root, E node) {
+        if (root != null) {
+            // like binary search
+            if (root.getData().compareTo(node) > 0) return search(root.getLeft(), node);
+            else if (root.getData().compareTo(node) < 0) return search(root.getRight(), node);
+            else return root;
+        } else return null;
+    }
+
+    //for depth method
+    public int findDepth(BNode<E> node) {
+        // if node have 2 child
+        if (node.getLeft() != null && node.getRight() != null) {
+            return Math.max(1 + findDepth(node.getLeft()), 1 + findDepth(node.getRight()));
+        }
+        // if node is leaf
+        else if (node.getLeft() == null && node.getRight() == null) return 0;
+            // if node have 1 child
+        else return (node.getLeft() == null) ? 1 + findDepth(node.getRight()) : 1 + findDepth(node.getLeft());
+    }
+
+    //for size method
     public int countSize(BNode<E> root) {
         return (root == null) ? 0 : 1 + countSize(root.getRight()) + countSize(root.getLeft());
     }
 
+    //for findMin method
     public E findMinWithBranch(BNode<E> e) {
         return (e.getLeft() == null) ? e.getData() : findMinWithBranch(e.getLeft());
     }
 
+    //for findMax method
     public E findMaxWithBranch(BNode<E> e) {
         return (e.getRight() == null) ? e.getData() : findMaxWithBranch(e.getRight());
     }
 
+    //for remove method
+    public boolean remove(E e) {
+        BNode<E> removeNode = search(this, e);
+        if (removeNode == null) return false;
+        else {
+            BNode<E> parent = findNodeParent(this, e);
+            //if Node don't have child
+            if (removeNode.getLeft() == null && removeNode.getRight() == null) {
+                if (parent.getData().compareTo(removeNode.getData()) < 0) parent.setRight(null);
+                else parent.setLeft(null);
+            }
+            //if Node have 1 child
+            else if (removeNode.getLeft() == null) parent.setRight(removeNode.getRight());
+            else if (removeNode.getRight() == null) parent.setLeft(removeNode.getLeft());
+                // if Node have 2 child
+            else {
+                BNode<E> newNode = findSuccessor(removeNode.getRight());
+                // recreate Successor node
+                remove(newNode.getData());
+                newNode.setLeft(removeNode.getLeft());
+                newNode.setRight(removeNode.getRight());
+                // add Successor node
+                if (parent.getData().compareTo(newNode.getData()) < 0) parent.setRight(newNode);
+                else parent.setLeft(newNode);
+            }
+        }
+        return true;
+    }
+
+    public BNode<E> findSuccessor(BNode<E> node) {
+        return (node.getLeft() == null) ? node : findSuccessor(node.getLeft());
+    }
+
+    public BNode<E> findNodeParent(BNode<E> node, E data) {
+        //if Node have 2 child
+        if (node.getLeft() != null && node.getRight() != null) {
+            if (node.getLeft().getData().compareTo(data) == 0 || node.getRight().getData().compareTo(data) == 0)
+                return node;
+            else {
+                BNode<E> leftVal = findNodeParent(node.getLeft(), data);
+                return (leftVal != null) ? leftVal : findNodeParent(node.getRight(), data);
+            }
+        }
+        //if Node have 0 child
+        else if (node.getLeft() == null && node.getRight() == null) return null;
+            //if Node have 1 child
+        else {
+            if (node.getLeft() == null) {
+                return (node.getRight().getData().compareTo(data) == 0) ? node : findNodeParent(node.getRight(), data);
+            } else return (node.getLeft().getData().compareTo(data) == 0) ? node : findNodeParent(node.getLeft(), data);
+        }
+    }
+
+    //for descendants method
     public List<E> addDescendants(BNode<E> node, List<E> list) {
         if (node != null) {
             list.add(node.getData());
@@ -65,6 +153,7 @@ public class BNode<E extends Comparable<E>> {
         } else return null;
     }
 
+    //for ancestor method
     public List<E> addAncestors(BNode<E> root, E data, List<E> list) {
         if (root == null) return null;
         else {
@@ -76,6 +165,7 @@ public class BNode<E extends Comparable<E>> {
         }
     }
 
+    //print BST
     public void inorderPrint(BNode<E> node) {
         if (node.getLeft() != null) inorderPrint(node.getLeft());
         System.out.print(node + " ");
--- BST.java
+++ BST.java
@@ -16,20 +16,8 @@ public class BST<E extends Comparable<E>> {
     // Task 1
     // Add element e into BST
     public void add(E e) {
-        add(this.root, e);
-    }
-
-    public void add(BNode<E> root, E e) {
         if (this.root == null) this.setRoot(new BNode<>(e));
-        else {
-            if (root.getData().compareTo(e) > 0) {
-                if (root.getLeft() == null) root.setLeft(new BNode<>(e));
-                else add(root.getLeft(), e);
-            } else {
-                if (root.getRight() == null) root.setRight(new BNode<>(e));
-                else add(root.getRight(), e);
-            }
-        }
+        else this.root.add(this.root, e);
     }
 
     // Add a collection of elements col into BST
@@ -39,35 +27,13 @@ public class BST<E extends Comparable<E>> {
 
     // compute the depth of a node in BST
     public int depth(E node) {
-        BNode<E> currentNode = search(this.root, node);
-        return (currentNode != null) ? findDepth(currentNode) : -1;
-    }
-
-    public BNode<E> search(BNode<E> root, E node) {
-        if (root != null) {
-            // like binary search
-            if (root.getData().compareTo(node) > 0) return search(root.getLeft(), node);
-            else if (root.getData().compareTo(node) < 0) return search(root.getRight(), node);
-            else return root;
-        } else return null;
-    }
-
-    public int findDepth(BNode<E> node) {
-        // if node have 2 child
-        if (node.getLeft() != null && node.getRight() != null) {
-            int leftDepth = 1 + findDepth(node.getLeft());
-            int rightDepth = 1 + findDepth(node.getRight());
-            return Math.max(leftDepth, rightDepth);
-        }
-        // if node is leaf
-        else if (node.getLeft() == null && node.getRight() == null) return 0;
-            // if node have 1 child
-        else return (node.getLeft() == null) ? 1 + findDepth(node.getRight()) : 1 + findDepth(node.getLeft());
+        BNode<E> currentNode = this.root.search(this.root, node);
+        return (currentNode != null) ? currentNode.findDepth(currentNode) : -1;
     }
 
     // compute the height of BST
     public int height() {
-        return findDepth(this.root);
+        return this.root.findDepth(this.root);
     }
 
     // Compute total nodes in BST
@@ -77,7 +43,7 @@ public class BST<E extends Comparable<E>> {
 
     // Check whether element e is in BST
     public boolean contains(E e) {
-        return search(this.root, e) != null;
+        return this.root.search(this.root, e) != null;
     }
 
     // Find the minimum element in BST
@@ -92,68 +58,18 @@ public class BST<E extends Comparable<E>> {
 
     // Remove element e from BST
     public boolean remove(E e) {
-        BNode<E> removeNode = search(this.root, e);
-        if (removeNode == null) return false;
-        else {
-            BNode<E> parent = findNodeParent(this.root, e);
-            //if Node don't have child
-            if (removeNode.getLeft() == null && removeNode.getRight() == null) {
-                if (parent.getData().compareTo(removeNode.getData()) < 0) parent.setRight(null);
-                else parent.setLeft(null);
-            }
-            //if Node have 1 child
-            else if (removeNode.getLeft() == null) parent.setRight(removeNode.getRight());
-            else if (removeNode.getRight() == null) parent.setLeft(removeNode.getLeft());
-                // if Node have 2 child
-            else {
-                BNode<E> newNode = findSuccessor(removeNode.getRight());
-                // recreate Successor node
-                remove(newNode.getData());
-                newNode.setLeft(removeNode.getLeft());
-                newNode.setRight(removeNode.getRight());
-                // add Successor node
-                if (parent.getData().compareTo(newNode.getData()) < 0) parent.setRight(newNode);
-                else parent.setLeft(newNode);
-            }
-        }
-        return true;
-    }
-
-    public BNode<E> findSuccessor(BNode<E> node) {
-        if (node.getLeft() == null) return node;
-        else return findSuccessor(node.getLeft());
-    }
-
-    public BNode<E> findNodeParent(BNode<E> node, E data) {
-        //if Node have 2 child
-        if (node.getLeft() != null && node.getRight() != null) {
-            if (node.getLeft().getData().compareTo(data) == 0 || node.getRight().getData().compareTo(data) == 0)
-                return node;
-            else {
-                BNode<E> leftVal = findNodeParent(node.getLeft(), data);
-                BNode<E> rightVal = findNodeParent(node.getRight(), data);
-                return (leftVal != null) ? leftVal : rightVal;
-            }
-        }
-        //if Node have 0 child
-        else if (node.getLeft() == null && node.getRight() == null) return null;
-            //if Node have 1 child
-        else {
-            if (node.getLeft() == null) {
-                return (node.getRight().getData().compareTo(data) == 0) ? node : findNodeParent(node.getRight(), data);
-            } else return (node.getLeft().getData().compareTo(data) == 0) ? node : findNodeParent(node.getLeft(), data);
-        }
+        return this.root.remove(e);
     }
 
     // get the descendants of a node
     public List<E> descendants(E data) {
-        BNode<E> currentNode = search(this.root, data);
+        BNode<E> currentNode = this.root.search(this.root, data);
         return (currentNode == null) ? null : currentNode.addDescendants(currentNode, new LinkedList<>());
     }
 
     // get the ancestors of a node
     public List<E> ancestors(E data) {
-        return (search(this.root, data) == null) ? null : this.root.addAncestors(this.root, data, new LinkedList<>());
+        return (this.root.search(this.root, data) == null) ? null : this.root.addAncestors(this.root, data, new LinkedList<>());
     }
 
     //Task 2
